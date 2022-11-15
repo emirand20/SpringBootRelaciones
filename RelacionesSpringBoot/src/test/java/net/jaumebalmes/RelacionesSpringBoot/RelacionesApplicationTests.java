7e6b61d61ad7fb30d238d77b3cb964f841fb2373
[emirand20] <[emirand20@jaumebalmes.net]>
2022-11-15 16:45:19 +0100

    part6

---
+++ RelacionesSpringBoot/src/test/java/net/jaumebalmes/RelacionesSpringBoot/RelacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package net.jaumebalmes.RelacionesSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
